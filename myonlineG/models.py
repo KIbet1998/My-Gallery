075d161398a89fbcd0a2b54ae471ba72fdc1fd61
Kibet 1998 <ronoronald1998@gmail.com>
2021-07-04 22:51:10 +0300

    add model image

--- myonlineG/models.py
+++ myonlineG/models.py
@@ -4,7 +4,26 @@ from django.db.models.base import Model
 # Create your models here.
 
 class Category(models.Model):
-    category_name=models.CharField(max_length=100,blank=True,null=True )
+    category=models.CharField(max_length=100,blank=True,null=True )
 
     def _str_(self):
-        return self.category_name
+        return self.category
+
+class Location(models.Model):
+    location=models.CharField(max_length=50,blank=False ,null=True)
+    
+
+    def _str_(self):
+        return self.location
+
+
+class Image(models.Model):
+    image=models.ImageField(null=True,blank=False)
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=4000)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    location=models.ForeignKey(Location,on_delete=models.CASCADE,null=True)
+
+    def _str_(self):
+        return self.name
+

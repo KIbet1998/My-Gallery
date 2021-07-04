1ec4c7522aca789357342159dbe719448eb61def
Kibet 1998 <ronoronald1998@gmail.com>
2021-07-04 22:56:13 +0300

    add image html file

--- myonlineG/urls.py
+++ myonlineG/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+   url('^$',views.gallery,name='gallery'),
+    path('photo/<str:pk>',views.viewPhoto,name='photo'),
+    url('add',views.addPhoto,name='add'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
